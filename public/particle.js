3734078132d461c081d75e8b14eacae2698b80cf
Sam Poder <sam.r.poder@gmail.com>
2025-03-08 03:50:21 -0600

    Update particle.js

--- public/particle.js
+++ public/particle.js
@@ -1,19 +1,19 @@
 class Particle {
   constructor(x, y, mass, id) {
-    this.position = createVector(x, y)
+    this.position = createVector(x, y) // refer to https://p5js.org/reference/p5/createVector/
     this.acceleration = createVector(0, 0)
     this.velocity = createVector(0, 0)
-    this.setMassMultiplier(5)
+    this.setMassMultiplier(5) // mass always stays the same but the "mass" slider changes this variable which artifically changes the mass. 
     this.setMass(mass)
-    this.id = id
+    this.id = id // unique ID used to identify particle
 
-    randomSeed(id * 9 / 3)
+    randomSeed(id * 9 / 3) // this way we can always get the right colour in our previews
     this.color = color(random(0, 255), random(0, 255), random(0, 255))
-    randomSeed((new Date()).getTime())
+    randomSeed((new Date()).getTime()) // for other random-ness we don't repeat
   }
 
   draw() {
-    if(this.mass >= 0) {
+    if(this.mass >= 0) { // "repeling" particles have a negative mass and our outlined
       noStroke()
       fill(this.color)
     } else {
@@ -40,15 +40,17 @@ class Particle {
     if (distance <= radius) return
 
     // force = G * mass1 * mass2 / distance ** 2
-    let force = p5.Vector.sub(this.position, particle.position)
-      .setMag(G * mass  / (distance ** 2))
+    let force = p5.Vector.sub(this.position, particle.position).setMag(G * mass  / (distance ** 2))
 
 
     particle.applyForce(force)
   }
 
   update() {
-    let deltaVelocity = p5.Vector.mult(this.acceleration, deltaTime)
+    // deltaTime, refer to https://p5js.org/reference/p5/deltaTime/
+
+    
+    let deltaVelocity = p5.Vector.mult(this.acceleration, deltaTime) // vector multiplication, velocity will be used to change the position
 
     this.velocity.set(this.velocity.add(deltaVelocity))
 
@@ -60,7 +62,7 @@ class Particle {
   }
   
   setMassMultiplier(multiplier) {
-    this.massMultiplier = multiplier
+    this.massMultiplier = multiplier // used to change the mass but indirect
   }
   
   setMass(mass) {
